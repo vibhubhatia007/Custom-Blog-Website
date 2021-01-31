210e2cd8329a882670f00650760fe78a5eea5313
Vibhu Bhatia <vibhubhatia0007@gmail.com>
2021-01-31 21:02:38 +0530

    combining components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-// import Home from "./components/Home"
 import About from "./components/About"
 import SinglePost from "./components/SinglePost"
 import Post from "./components/Post"
